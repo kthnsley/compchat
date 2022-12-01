631d3d16b43b65df84fe240005090df87e5a2be7
tacticaltux <dylhens03@gmail.com>
2022-11-30 22:53:28 -0600

    reorganize some files

--- tests/run_server_with_ssl.py
+++ tests/run_server_with_ssl.py
@@ -8,4 +8,4 @@ os.environ["COMPCHAT_SSL_KEY"] = OUR_PATH + "/ssl/example.key"
 os.environ["COMPCHAT_SSL_CRT"] = OUR_PATH + "/ssl/example.crt"
 os.environ["COMPCHAT_SSL_ENABLED"] = "1"
 
-import run_core
+import run_server
